8d078796be9820c12a564a77a5d422603cd1e9cf
soudhaa <soudha.aitrich@gmail.com>
2023-10-20 16:45:50 +0530

    Add project files.

---
+++ Domain/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HireMeNow_WebApi.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+        {
+            //services.AddDbContext<ApplicationDbContext>(options =>
+            //    options.UseSqlServer(config.GetConnectionString("DefaultConnection"))
+            //);
+            return services;
+        }
+    }
+}
---
+++ HireMeNow_WebApi/JobSeeker/JobSeekerController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HireMeNow_WebApi.JobSeeker
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class JobSeekerController : ControllerBase
+	{
+	}
+}
