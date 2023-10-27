3199e7e5a882a394704d0102d187273a109feb18
soudhaa <soudha.aitrich@gmail.com>
2023-10-27 15:44:42 +0530

    commit

--- Domain/Models/DbHireMeNowWebApiContext.cs
+++ Domain/Models/DbHireMeNowWebApiContext.cs
@@ -52,12 +52,9 @@ public partial class DbHireMeNowWebApiContext : DbContext
 
 	public virtual DbSet<WorkExperience> WorkExperiences { get; set; }
 
-	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-
-
-      => optionsBuilder.UseSqlServer("Data Source=RAZIYA;Initial Catalog=HireMeNowDbNew;Integrated Security=True;Persist Security Info=True;Trust Server Certificate=True;");
-
-      
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=DESKTOP-PBRNQVI;Initial Catalog=WebApi_DB;User ID=soudha;Password=soudha;Integrated Security=True;Trusted_Connection=True;TrustServerCertificate=true;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
